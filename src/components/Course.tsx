ddcd5672fb20ca980ac3c4c462e4c7408d82763d
jwdagney55 <jwdagney@udel.edu>
2021-10-14 20:43:53 -0400

    Adding Course and Semester Components

---
+++ src/components/Course.tsx
@@ -0,0 +1,9 @@
+import react from 'react'
+
+function Course({id, name, description} : {id:string, name:string, description:string}): JSX.Element {
+    return (
+        <p>This is a Course</p>
+    );
+}
+
+export default Course;
---
+++ src/components/Semester.tsx
@@ -0,0 +1,9 @@
+
+
+function Semester(): JSX.Element {
+    return (
+        <p>This is a Semester</p>
+    );
+}
+
+export default Semester;
