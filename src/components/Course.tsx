944a54940bd1c7903b9ea865d05ff1c0d5b072d5
jwdagney55 <jwdagney@udel.edu>
2021-10-14 20:59:35 -0400

    yeah

--- src/components/Course.tsx
+++ src/components/Course.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 function Course({id, name, description} : {id:string, name:string, description:string}): JSX.Element {
+    
     return (
-        <p>This is a Course</p>
+        <p>This is a Course with id: {id}, name: {name}, and description: {description}</p>
     );
 }
 
