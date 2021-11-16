ec37b215704ff55c529e5a7b614af387ede19d7c
jwdagney55 <jwdagney@udel.edu>
2021-11-16 16:43:11 -0500

    Make linter happy

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -17,20 +17,20 @@ describe("App", () => {
     it("renders UD CISC Scheduler text", () => {
         const linkElement = screen.getByText(/UD CIS Scheduler/i);
         expect(linkElement).toBeInTheDocument();
-    })
+    });
 
     it("has the Add New Course button on render", () => {
         const element = screen.queryByTestId("addCourse");
         expect(element).toBeInTheDocument();
-    })
+    });
 
     it("renders Add Course Modal on click", async () => {
-        const button = screen.queryByTestId('addCourse');
+        const button = screen.queryByTestId("addCourse");
         if(button !== null){
             button.click();
             const element = await screen.queryByTestId("DeptSearch");
             expect(element).toBeInTheDocument();
         }
 
-    })
-})
+    });
+});
