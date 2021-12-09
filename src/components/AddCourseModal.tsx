6d7bcdab691e1d7697a9800e926ca3210b6e0e17
jwdagney55 <jwdagney@udel.edu>
2021-12-08 23:33:01 -0500

    can you hear me? ACM line 35

--- src/components/AddCourseModal.tsx
+++ src/components/AddCourseModal.tsx
@@ -32,6 +32,7 @@ export function AddCourseModal({currClasses, visible, setVisible, setCurrCourse,
         setCurrCourse(newClasses.concat(newClass));
         addlistOfCourseLists(newClass);
         hide();
+        console.log("can you hear me?");
     }
     const hide = () => {
         console.log(errorAddCourse);
